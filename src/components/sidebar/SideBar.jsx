b0815ae98104a3048f648bddc38f11c81822fe8f
mohammad <mdfa1381@gmail.com>
2023-01-25 01:45:57 +0330

    fix sidebar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -12,6 +12,8 @@ import {
   MessageOutlined,
 } from '@mui/icons-material'
 
+import { Link } from 'react-router-dom'
+
 import './SideBar.css'
 function SideBar() {
   return (
@@ -21,10 +23,12 @@ function SideBar() {
           <div className="sideBarMenu">
             <h3 className="sideBarTitle">Dashboard</h3>
             <ul className="sideBarList">
-              <li className="sideBarListItem">
-                <LineStyle className="sideBarIcon" />
-                Home
-              </li>
+              <Link to="/" className="link">
+                <li className="sideBarListItem">
+                  <LineStyle className="sideBarIcon" />
+                  Home
+                </li>
+              </Link>
               <li className="sideBarListItem">
                 <Timeline className="sideBarIcon" />
                 Analytics
@@ -36,22 +40,30 @@ function SideBar() {
             </ul>
             <h3 className="sideBarTitle">Quick Menu</h3>
             <ul className="sideBarList">
-              <li className="sideBarListItem">
-                <PermIdentity className="sideBarIcon" />
-                Users
-              </li>
-              <li className="sideBarListItem">
-                <PermIdentity className="sideBarIcon" />
-                New User
-              </li>
-              <li className="sideBarListItem">
-                <Storefront className="sideBarIcon" />
-                Products
-              </li>
+              <Link to="/users" className="link">
+                <li className="sideBarListItem">
+                  <PermIdentity className="sideBarIcon" />
+                  Users
+                </li>
+              </Link>
+              <Link to="/newUser" className="link">
+                <li className="sideBarListItem">
+                  <PermIdentity className="sideBarIcon" />
+                  New User
+                </li>
+              </Link>
+              <Link to="/products " className="link">
+                <li className="sideBarListItem">
+                  <Storefront className="sideBarIcon" />
+                  Products
+                </li>
+              </Link>
+
               <li className="sideBarListItem">
                 <AttachMoney className="sideBarIcon" />
                 TransActions
               </li>
+
               <li className="sideBarListItem">
                 <BarChart className="sideBarIcon" />
                 Reports
@@ -63,10 +75,12 @@ function SideBar() {
                 <MailOutline className="sideBarIcon" />
                 Mail
               </li>
+
               <li className="sideBarListItem">
                 <DynamicFeed className="sideBarIcon" />
                 FeedBack
               </li>
+
               <li className="sideBarListItem">
                 <MessageOutlined className="sideBarIcon" />
                 Message
