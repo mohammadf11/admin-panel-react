d448cf2075a8dc5c3e27e2c857025e66b63ba238
mohammad <mdfa1381@gmail.com>
2023-01-25 01:18:16 +0330

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
 import Topbar from "./components/topbar/Topbar";
-
+import SideBar from "./components/sidebar/SideBar";
+import './App.css'
 function App() {
   let router = useRoutes(routes);
   return (
     <>
       <Topbar />
-      {router}
+      <div className="container">
+        <SideBar />
+        {router}
+      </div>
     </>
   );
 }
