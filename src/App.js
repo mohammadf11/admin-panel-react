135cb368acc8dffdbda46d47ff3c461ec8165f45
mohammad <mdfa1381@gmail.com>
2023-01-25 00:11:19 +0330

    topbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   let router = useRoutes(routes);
-  return <>{router}</>;
-} 
+  return (
+    <>
+      <Topbar />
+      {router}
+    </>
+  );
+}
 
 export default App;
